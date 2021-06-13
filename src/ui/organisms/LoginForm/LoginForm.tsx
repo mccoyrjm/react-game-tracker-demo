b83b3cb9553316834007e8c75762918e22ffb051
Ryan McCoy <mccoyrjm@gmail.com>
2021-06-13 18:42:09 -0400

    Add LoginForm component

---
+++ src/ui/organisms/LoginForm/LoginForm.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from 'react'
+import Button from 'ui/atoms/Button'
+import TextField from 'ui/atoms/TextField'
+import HorizontalBreak from 'ui/HorizontalBreak'
+import { Form } from './subcomponents'
+
+const LoginForm = (): ReactElement => {
+  return (
+    <Form>
+      <TextField />
+      <TextField />
+      <HorizontalBreak />
+      <Button isPrimary onClick={() => { console.log('login click') }}>Login</Button>
+    </Form>
+  )
+}
+
+export default LoginForm

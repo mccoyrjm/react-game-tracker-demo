957fe818550b20b3ef552c996ed5b2d213aa9c7d
Ryan McCoy <mccoyrjm@gmail.com>
2021-06-13 17:17:51 -0400

    Setup react router

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,7 +1,25 @@
 import { ReactElement } from 'react'
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route
+} from 'react-router-dom'
+import Header from 'ui/organisms/Header'
+import Footer from 'ui/organisms/Footer'
+import Login from 'pages/Login'
 
 const App = (): ReactElement => {
-  return <div>Hello World!</div>
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App

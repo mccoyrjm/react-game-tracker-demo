259a31b7222142cc98a4512adbc135f233b25182
Ryan McCoy <mccoyrjm@gmail.com>
2021-06-13 18:41:15 -0400

    Add media breakpoints

---
+++ src/styles/mediaQueries.ts
@@ -0,0 +1,19 @@
+const unit = 'px'
+
+// Size names based on DnD Terminology!
+// https://dungeons.fandom.com/wiki/SRD:Table_of_Creature_Size_and_Scale
+const breakPoints = {
+  tiny: 320,
+  small: 480,
+  medium: 768,
+  large: 992
+}
+
+const greaterThan = {
+  greaterThanTiny: `min-width: ${breakPoints.tiny}${unit}`,
+  greaterThanSmall: `min-width: ${breakPoints.small}${unit}`,
+  greaterThanMedium: `min-width: ${breakPoints.medium}${unit}`,
+  greaterThanLarge: `min-width: ${breakPoints.large}${unit}`
+}
+
+export default greaterThan
